50267d138707290132087bd57e38e88cc6901273
Porimol Chandro <i-porimal.chandro@ekkdam.com>
2018-04-10 16:08:42 +0600

    endpoints name changed

--- modules/posts/routes/__init__.py
+++ modules/posts/routes/__init__.py
@@ -8,30 +8,30 @@ post_routes = Blueprint("post_routes", __name__, url_prefix="/{0}".format(API_VE
 
 post_routes.add_url_rule(
     "/posts",
-    endpoint="index",
+    endpoint="get_all_post",
     view_func=get_all_post,
     methods=['GET'])
 
 post_routes.add_url_rule(
     "/posts",
-     endpoint="create",
+     endpoint="create_post",
      view_func=create_post,
      methods=['POST'])
 
 post_routes.add_url_rule(
     "/posts/<id>",
-    endpoint="get",
+    endpoint="get_post",
     view_func=get_post,
     methods=['GET'])
 
 post_routes.add_url_rule(
     "/posts/<id>",
-    endpoint="update",
+    endpoint="update_post",
     view_func=update_post,
     methods=['PUT'])
 
 post_routes.add_url_rule(
     "/posts/<id>",
-    endpoint="delete",
+    endpoint="delete_post",
     view_func=delete_post,
     methods=['DELETE'])
